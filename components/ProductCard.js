1ffd29d6055c0f96ed124b01f59cb309129a3053
Miraj <mirajaryal@gmail.com>
2021-09-28 14:52:06 +1000

    Fallback logic for broken image added

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const ProductCard = ( { index, isSale, price, productImage, productName, type } ) =>
 {
+    const addDefaultSrc = ( e ) =>
+    {
+        e.target.src = `https://source.unsplash.com/collection/${ index }/512x512`;
+    };
     return (
         <div className="w-full">
-            {productName}
+            <img onError={( e ) => addDefaultSrc( e )} className="responsive" src={productImage} alt={productName} />
+            <div className="flex items-center justify-between gap-4">
+                <div className="text-base text-gray-900">{productName}</div>
+                <div className="text-base text-gray-900">{price}</div>
+            </div>
         </div>
     );
 };
