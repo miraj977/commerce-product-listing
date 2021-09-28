2f8c092a0cb3f70a770e7258a9f1370366932e5d
Miraj <mirajaryal@gmail.com>
2021-09-28 18:01:46 +1000

    Dynamic routing changed from StaticProps to ServerSideProps due to local api being passed

--- components/Image.js
+++ components/Image.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Image = ( { size = "512x512", url, index = 0, alt = "Product Image", addClass } ) =>
+const Image = ( { size = "512x512", url, index = 0, alt = "Product Image", addClass, random } ) =>
 {
+    const randomUrl = `https://source.unsplash.com/collection/${ index }/${ size }`;
     const addDefaultSrc = ( e ) =>
     {
-        e.target.src = `https://source.unsplash.com/collection/${ index }/${ size }`;
+        e.target.src = randomUrl;
     };
     return (
-        <img onError={( e ) => addDefaultSrc( e )} className={`${ addClass }`} src={url} alt={alt} />
+        <img onError={( e ) => addDefaultSrc( e )} className={`${ addClass }`} src={random ? randomUrl : url} alt={alt} />
     );
 };
 
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import doAxios from '@lib/doAxios';
 import Loading from '@components/Loading';
 import Container from '@components/Layout/Container';
 import Image from '@components/Image';
+import Router from 'next/router';
+
 const Index = ( { product } ) =>
 {
     const { productName, price, isSale, index, productImage, type } = product[ 0 ];
@@ -11,10 +13,22 @@ const Index = ( { product } ) =>
 
     return (
         <Container addClass="py-8">
-            {productName && <h1 className="text-2xl font-bold">{productName}</h1>}
-            <Image url={productImage} size="1600x800" alt={productName} addClass="w-full my-4" />
-            {price && <p className="text-base font-bold">{price}</p>}
-            <p className="my-4 text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, a aperiam. Odio ratione beatae recusandae quidem iste tempore, libero, maiores sint quis animi minima sunt dolorum? Nesciunt veritatis hic, dolor voluptatem sint voluptatibus rerum iure tempore, quos suscipit magni odit molestiae vel impedit ad fugit non consequatur? Tempore placeat repellat vitae blanditiis porro rem aliquam magni sit impedit minima tenetur ratione, earum, doloremque sed, in fugit inventore. Nesciunt, illum, aliquid sapiente dolorum ullam est, quos facere consequatur laboriosam ipsa praesentium.</p>
+            <p className="text-sm underline cursor-pointer" onClick={() => Router.back()}>
+                Go back
+            </p>
+
+            {productName &&
+                <h1 className="pt-3 pb-5 text-2xl font-bold">{productName}</h1>}
+
+
+            <Image url={productImage} random size="1600x800" alt={productName} addClass="w-full my-4" />
+
+            {price &&
+                <p className="text-base font-bold">{price}</p>}
+
+            <p className="my-5 text-base">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, a aperiam. Odio ratione beatae recusandae quidem iste tempore, libero, maiores sint quis animi minima sunt dolorum? Nesciunt veritatis hic, dolor voluptatem sint voluptatibus rerum iure tempore, quos suscipit magni odit molestiae vel impedit ad fugit non consequatur? Tempore placeat repellat vitae blanditiis porro rem aliquam magni sit impedit minima tenetur ratione, earum, doloremque sed, in fugit inventore. Nesciunt, illum, aliquid sapiente dolorum ullam est, quos facere consequatur laboriosam ipsa praesentium.
+            </p>
         </Container>
     );
 };
@@ -22,27 +36,10 @@ const Index = ( { product } ) =>
 export default Index;
 
 
-// Collecting all the possible paths 
-export async function getStaticPaths ()
-{
-    const request = await doAxios( `${ process.env.baseUrl }/api/products` );
-    const products = await request.data;
-
-    const paths = products.map( ( product ) => ( {
-        params: { slug: product.index.toString() }
-    } ) );
-
-    return {
-        paths,
-        fallback: false
-    };
-}
-
 // Getting the slug specific data and passing as props
-export async function getStaticProps ( { params } )
+export async function getServerSideProps ( context )
 {
-    const { slug } = params;
-
+    const { slug } = context.query;
     const request = await doAxios( `${ process.env.baseUrl }/api/product/${ slug }` );
     const product = await request.data;
 
