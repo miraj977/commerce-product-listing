8f46e761135c09b2a8d3420de1bba29eca271314
Miraj <mirajaryal@gmail.com>
2021-10-24 23:03:53 +1100

    loading state added

--- components/Partials/Nav.js
+++ components/Partials/Nav.js
@@ -42,7 +42,7 @@ const Nav = () =>
 
     useEffect( () =>
     {
-        const timer = setTimeout( () => { setIsLoading( false ); }, 1500 );
+        const timer = setTimeout( () => { setIsLoading( false ); }, 600 );
         //cleanup function
         return () => clearTimeout( timer );
     }, [ isLoading ] );
